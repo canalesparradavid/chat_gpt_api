0fe0231c18cbfeccaa6f0966b1ac8ef707f0ea13
David Canales Parra <canalesparradavid@gmail.com>
2023-05-22 00:53:58 +0200

    Añadida limitacion de mensajes a 10 y de maximo de caracteres almacenados

--- js/MSGRepository.js
+++ js/MSGRepository.js
@@ -1,13 +1,30 @@
+const MAX_MSGS = 10;
+const MAX_TOKENS = 2800;
+
 class MSGRepository {
     constructor() {
+        this.tokens = 0;
         this.msgs = [];
     }
 
     add(msg) {
+        this.tokens += msg.msg.length;
         this.msgs.push({
             "role": msg.role,
             "content": msg.msg
         });
+
+        // Si se sobrepasa el maximo de mensajes elimino el primero
+        if (this.msgs.length == MAX_MSGS + 1) {
+            this.tokens -= this.msgs[1].length;
+            this.msgs.splice(1,1);
+        }
+
+        // Si se sobrepasa el maximo de tokens almacenables elimino mensajes
+        while (this.tokens > MAX_TOKENS) {
+            this.tokens -= this.msgs[1].length;
+            this.msgs.splice(1,1);
+        }
     }
 
     getAll() {
