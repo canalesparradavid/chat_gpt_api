75cbd49127bdc570bbbf297f27d084691870fa8c
David Canales Parra <canalesparradavid@gmail.com>
2023-05-22 01:05:58 +0200

    Comprobada respuesta de DataSource correcta para almacenamiento de mensajes

--- js/MSGController.js
+++ js/MSGController.js
@@ -16,11 +16,15 @@ class MSGController {
         // Hago la peticion a la API de ChatGPT
         var msgs = this.msgRepository.getAll();
         var response = this.chatGPTDataSource.chat(msgs);
+        var msg_text = response[0];
+        var status_code = response[1];
 
         // Almaceno la respuesta en el repositorio
-        this.msgRepository.add(new MSG("assistant", response));
+        if (status_code == 200) {
+            this.msgRepository.add(new MSG("assistant", msg_text));
+        }
 
-        return response;
+        return msg_text;
     }
 }
 
